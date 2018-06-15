ba1167829419b475c5654ba91357feb99e666616
Cooper <18910890210@163.com>
2018-06-16 00:50:04 +0800

    第一个版本 无限listview

--- lib/main.dart
+++ lib/main.dart
@@ -6,18 +6,105 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random(); // Add this line.
     return new MaterialApp(
-      title: 'Welcome to Flutter',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Welcome to Flutter'),
-        ),
-        body: new Center(    // Change "const" to "new".
-          //child: const Text('Hello World'),   // Replace this text...
-          child: new Text(wordPair.asCamelCase),  // With this text.
-        ),
+      home: new RandomWords(),
+      theme: new ThemeData(
+        primaryColor: Colors.blueGrey
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  EnglishWordsState createState() => new EnglishWordsState();
+}
+
+class EnglishWordsState extends State<RandomWords> {
+    final List<WordPair> _wordList = <WordPair>[];
+    final Set<WordPair> _saved = new Set<WordPair>();
+    final TextStyle _style = const TextStyle(fontSize: 18.0);
+
+    @override
+    Widget build(BuildContext context) {
+      return new Scaffold(
+        appBar: new AppBar(
+          title: const Text('123'),
+          actions: <Widget>[
+            new IconButton(icon: const Icon(Icons.ac_unit), onPressed: _pushSaved)
+          ],
+        ),
+        body: _getWordList(),
+      );
+    }
+
+    void _pushSaved () {
+      Navigator.of(context).push(
+        new MaterialPageRoute<void>(
+          builder: (BuildContext context) {
+            final Iterable<ListTile> tiles = _saved.map(
+              (WordPair pair) {
+                return new ListTile(
+                  title: new Text(
+                    pair.asPascalCase,
+                    style: _style,
+                  ),
+                );
+              },
+            );
+            final List<Widget> divided = ListTile
+              .divideTiles(
+                context: context,
+                tiles: tiles,
+              )
+              .toList();
+              return new Scaffold(
+                appBar: new AppBar(
+                  title: const Text('faverate'),
+                ),
+                body: new ListView(children: divided),
+              );
+          },
+          
+        )
+      );
+    }
+    Widget _getWordList() {
+      return new ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (BuildContext _context, int i){
+          if (i.isOdd) {
+            return new Divider();
+          }
+          final int index = i ~/ 2;
+          if (index >= _wordList.length) {
+            _wordList.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_wordList[index]);
+        },
+      );
+    }
+
+    Widget _buildRow(WordPair pair) {
+      final bool alreadySaved = _saved.contains(pair);
+      return new ListTile(
+        title: new Text(
+          pair.asPascalCase,
+          style: _style,
+        ),
+        trailing: new Icon(   // Add the lines from here... 
+          alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.blue[300] : null,
+        ),    
+        onTap: (){
+          setState(() {
+            if (alreadySaved) {
+              _saved.remove(pair);
+            } else {
+              _saved.add(pair);
+            }
+          });
+        },
+      );
   }
 }
