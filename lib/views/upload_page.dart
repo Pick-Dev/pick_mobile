ff8de30cf94303c1fc5ed76ef00f52788091ab13
Cooper <18910890210@163.com>
2018-07-02 19:37:35 +0800

    update info.plist for iOS

--- lib/views/upload_page.dart
+++ lib/views/upload_page.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class UploadPage extends StatefulWidget {
 
@@ -9,6 +13,16 @@ class UploadPage extends StatefulWidget {
 
 class _UploadPageState extends State<UploadPage> {
 
+  File _image;
+
+  Future getImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      _image = image;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -17,8 +31,13 @@ class _UploadPageState extends State<UploadPage> {
             title: new Text('读图上传图片'),
           ),
           body: new Center(
-              child: null
-          ),
+            child: _image == null ? new Text('No image selected.') : new Image.file(_image),
+        ),
+        floatingActionButton: new FloatingActionButton(
+          onPressed: getImage,
+          tooltip: 'Pick Image',
+          child: new Icon(Icons.add_a_photo),
+        ),
         ),
     );
   }
